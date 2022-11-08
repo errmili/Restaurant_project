0bb0f077189de54ea5434376e8f73ec78d6c071a
errmili <96416928+errmili@users.noreply.github.com>
2022-11-08 16:35:08 +0100

    create a model (cayegory & order) closes #6

--- back-end/src/main/java/com/spring/restaurant/DemoApplication.java
+++ back-end/src/main/java/com/spring/restaurant/DemoApplication.java
@@ -2,6 +2,7 @@ package com.spring.restaurant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import src.main.java.com.spring.restaurant.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
---
+++ back-end/src/main/java/com/spring/restaurant/model/Category.java
@@ -0,0 +1,7 @@
+package com.spring.restaurant.model;
+
+public class Category {
+
+    private Long id;
+    private  String CategoryName;
+}
