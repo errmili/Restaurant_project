f765653d2a64b8d6d805e98108add078a3ee18da
errmili <96416928+errmili@users.noreply.github.com>
2022-11-09 15:56:14 +0100

    add a package service with classes closes #13

---
+++ back-end/src/main/java/com/spring/restaurant/service/OrderService.java
@@ -0,0 +1,17 @@
+package com.spring.restaurant.service;
+
+import com.spring.restaurant.dao.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository){
+        this.orderRepository =  orderRepository;
+    }
+
+}
