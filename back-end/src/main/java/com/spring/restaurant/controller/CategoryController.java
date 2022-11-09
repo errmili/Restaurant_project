d81a0ff870b2b98bfd544b9b90ae8e064b146fee
errmili <96416928+errmili@users.noreply.github.com>
2022-11-09 16:13:20 +0100

    add a package controller with classes  closes #14

---
+++ back-end/src/main/java/com/spring/restaurant/controller/CategoryController.java
@@ -0,0 +1,17 @@
+package com.spring.restaurant.controller;
+
+import com.spring.restaurant.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//psk on vas travailler avec une API
+public class CategoryController {
+
+    CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+}
